dd5dcef9a7955550227c014d3094054dffea2433
WitherRedstone <1751216077@qq.com>
2026-04-01 13:57:41 +0800

    完善末地内容

--- src/main/java/com/chinaex123/kaleidoscope_dim_wine/ModCreativeTabs.java
+++ src/main/java/com/chinaex123/kaleidoscope_dim_wine/ModCreativeTabs.java
@@ -20,32 +20,42 @@ public class ModCreativeTabs {
                     .title(Component.translatable("itemGroup.kaleidoscope_dim_wine_tab"))
                     .displayItems((parameters, output) -> {
 
-                        // -------------------- 流体桶 --------------------
+                        // ==================== 流体桶 ====================
+                        // -------------------- 下界 --------------------
                         output.accept(ModItems.CRIMSON_GRAPE_JUICE_BUCKET.get()); // 绯红葡萄汁桶
                         output.accept(ModItems.WARPED_GRAPE_JUICE_BUCKET.get()); // 诡异葡萄汁桶
-
+                        // -------------------- 末地 --------------------
                         output.accept(ModItems.DREAMFRUIT_JUICE_BUCKET.get()); // 迷梦果汁桶
                         output.accept(ModItems.DRAGON_BLOOD_BUCKET.get()); // 龙血桶
-                        // -------------------- 作物 --------------------
+
+
+                        // ==================== 作物 ====================
+                        // -------------------- 下界 --------------------
                         output.accept(ModItems.CRIMSON_GRAPEVINE.get()); // 绯红葡萄藤
                         output.accept(ModItems.WARPED_GRAPEVINE.get()); // 诡异葡萄藤
                         output.accept(ModItems.CRIMSON_GRAPE.get()); // 绯红葡萄
                         output.accept(ModItems.WARPED_GRAPE.get()); // 诡异葡萄
-
+                        // -------------------- 末地 --------------------
                         output.accept(ModItems.DREAMFRUIT.get()); // 迷梦果
-                        // -------------------- 物品 --------------------
+
+
+                        // ==================== 物品 ====================
+                        // -------------------- 下界 --------------------
                         output.accept(ModItems.CRIMSON_FUNGAL_SAP.get()); // 绯红菌露
                         output.accept(ModItems.WARPED_FUNGAL_SAP.get()); // 诡影菌髓
-
+                        // -------------------- 末地 --------------------
                         output.accept(ModItems.DRAGON_BLOOD_BOTTLE.get()); // 龙血瓶
-                        // -------------------- 酒类 --------------------
+
+
+                        // ==================== 酒类 ====================
+                        // -------------------- 下界 --------------------
                         output.accept(ModItems.CRIMSON_CRESCENDO.get()); // 绯红绝响
                         output.accept(ModItems.ETHEREAL_NOBLE.get()); // 诡影尊爵
                         output.accept(ModItems.WART_HENNESSY.get()); // 疣域轩尼诗
                         output.accept(ModItems.BLAZE_MARTELL.get()); // 烈焰马爹利
                         output.accept(ModItems.MAGMA_ROYAL_SALUTE.get()); // 岩浆皇家礼炮
                         output.accept(ModItems.BLACK_BONE_LAFITE.get()); // 黑骨拉菲
-
+                        // -------------------- 末地 --------------------
                         output.accept(ModItems.CHORUS_COGNAC.get()); // 紫颂干邑
                         output.accept(ModItems.DRAGONS_BREATH_BRANDY.get()); // 龙息白兰地
                         output.accept(ModItems.ABYSSAL_PORTER.get()); // 深渊波特
@@ -55,6 +65,10 @@ public class ModCreativeTabs {
                         output.accept(ModItems.END_STARLIGHT.get()); // 终末星光
                         output.accept(ModItems.DIVINE_EMBRYO_PORT.get()); // 神血波尔多
                         output.accept(ModItems.DRACONIC_BLOOD_WINE.get()); // 龙血酒
+
+
+
+
 
                     })
                     .build());
