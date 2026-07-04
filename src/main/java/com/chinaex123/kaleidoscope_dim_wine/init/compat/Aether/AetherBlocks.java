eb28824b5e581d396f37ace77e76205a534f3c4b
WitherRedstone <1751216077@qq.com>
2026-07-04 11:20:52 +0800

    1.1.2

--- src/main/java/com/chinaex123/kaleidoscope_dim_wine/init/compat/Aether/AetherBlocks.java
+++ src/main/java/com/chinaex123/kaleidoscope_dim_wine/init/compat/Aether/AetherBlocks.java
@@ -17,12 +17,12 @@ public interface AetherBlocks {
 
     // -------------------- 酒类 --------------------
     // 酒类
-    RegistryObject<Block> DIVINE_OFFERING_BREW = BLOCK_REGISTER.register("divine_offering_brew", () -> DrinkBlock.create().maxCount(2).irregular().shapes(DrinkShapes.getShortBottleShapes()).build().get());
+    RegistryObject<Block> DIVINE_OFFERING_BREW = BLOCK_REGISTER.register("divine_offering_brew", () -> DrinkBlock.create().maxCount(2).shapes(DrinkShapes.getShortBottleShapes()).build().get());
     RegistryObject<Block> DIVINE_ENERGY_MIST = BLOCK_REGISTER.register("divine_energy_mist", () -> DrinkBlock.create().maxCount(4).shapes(DrinkShapes.getShortBottleShapes()).build().get());
     RegistryObject<Block> SWET_FIZZ = BLOCK_REGISTER.register("swet_fizz", () -> DrinkBlock.create().maxCount(4).shapes(DrinkShapes.getShortBottleShapes()).build().get());
     RegistryObject<Block> GRAVITITE_DRUNK = BLOCK_REGISTER.register("gravitite_drunk", () -> DrinkBlock.create().maxCount(4).shapes(DrinkShapes.getShortBottleShapes()).build().get());
     RegistryObject<Block> HEALING_ELIXIR = BLOCK_REGISTER.register("healing_elixir", () -> DrinkBlock.create().maxCount(4).shapes(DrinkShapes.getShortBottleShapes()).build().get());
-    RegistryObject<Block> GINGERBREAD_WARMER = BLOCK_REGISTER.register("gingerbread_warmer", () -> DrinkBlock.create().maxCount(2).irregular().shapes(DrinkShapes.getShortBottleShapes()).build().get());
+    RegistryObject<Block> GINGERBREAD_WARMER = BLOCK_REGISTER.register("gingerbread_warmer", () -> DrinkBlock.create().maxCount(2).shapes(DrinkShapes.getShortBottleShapes()).build().get());
     RegistryObject<Block> UNBOUND_SKYBORN = BLOCK_REGISTER.register("unbound_skyborn", () -> DrinkBlock.create().maxCount(4).shapes(DrinkShapes.getShortBottleShapes()).build().get());
 
     static void register(IEventBus eventBus) {
